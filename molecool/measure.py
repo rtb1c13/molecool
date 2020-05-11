10db2ab776c35caba9f7761d2ed00b71a55becb3
rtb1c13 <Richard.Bradshaw@kcl.ac.uk>
2020-05-11 12:08:39 +0100

    run yapf on molecool

--- molecool/measure.py
+++ molecool/measure.py
@@ -4,6 +4,7 @@ Functions to measure/calculate values from structures
 
 import numpy as np
 
+
 def calculate_distance(coords_A, coords_B):
     """Calculate the distance between two points.
 
@@ -24,22 +25,21 @@ def calculate_distance(coords_A, coords_B):
     >>> calculate_distance(r1, r2)
     0.1
     """
-
+    
     difference_vec = (coords_A - coords_B)
     distance = np.linalg.norm(difference_vec)
 
     return distance
+
 
 def calculate_angle(rA, rB, rC, degrees=False):
     # Calculate the angle between three points. Answer is given in radians by default, but can be given in degrees
     # by setting degrees=True
     AB = rB - rA
     BC = rB - rC
-    theta=np.arccos(np.dot(AB, BC)/(np.linalg.norm(AB)*np.linalg.norm(BC)))
+    theta = np.arccos(np.dot(AB, BC) / (np.linalg.norm(AB) * np.linalg.norm(BC)))
 
     if degrees:
         return np.degrees(theta)
     else:
         return theta
-
-
