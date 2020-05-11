7afa65afcbe386a175e2e269cd10592f930128ec
rtb1c13 <Richard.Bradshaw@kcl.ac.uk>
2020-05-11 11:32:52 +0100

    Edited style of calculate_distance function

--- molecool/measure.py
+++ molecool/measure.py
@@ -4,11 +4,31 @@ Functions to measure/calculate values from structures
 
 import numpy as np
 
-def calculate_distance(rA, rB):
-    # This function calculates the distance between two points given as numpy arrays.
-    d=(rA-rB)
-    dist=np.linalg.norm(d)
-    return dist
+def calculate_distance(coords_A, coords_B):
+    """Calculate the distance between two points.
+
+    Parameters
+    ----------
+    coords_A, coords_B : np.ndarray
+        The coordinates of each point.
+
+    Returns
+    -------
+    distance : float
+        The distance between the two points.
+    
+    Examples
+    --------
+    >>> r1 = np.array([0, 0, 0])
+    >>> r2 = np.array([0, 0.1, 0])
+    >>> calculate_distance(r1, r2)
+    0.1
+    """
+
+    difference_vec = (coords_A - coords_B)
+    distance = np.linalg.norm(difference_vec)
+
+    return distance
 
 def calculate_angle(rA, rB, rC, degrees=False):
     # Calculate the angle between three points. Answer is given in radians by default, but can be given in degrees
